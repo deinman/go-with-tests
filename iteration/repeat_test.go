e1eb06e6c6295bb9d64d12f5079570e5c7697351
deinman <>
2023-02-05 21:05:38 +0000

    Additional tests.

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -2,25 +2,39 @@ package iteration
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
+	t.Run("0 times produces empty string", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := ""
+		assertCorrectMessage(t, repeated, expected)
+	})
 	t.Run("repeat a 5 times", func(t *testing.T) {
-		repeated := Repeat("a")
+		repeated := Repeat("a", 5)
 		expected := "aaaaa"
 		assertCorrectMessage(t, repeated, expected)
+	})
+	t.Run("character count matches expected times", func(t *testing.T) {
+		const count = 5
+		repeated := Repeat("a", count)
+		found := strings.Count(repeated, "a")
+		if count != found {
+			t.Errorf("expected %d but found %d", count, found)
+		}
 	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
 
 func ExampleRepeat() {
-	result := Repeat("a")
+	result := Repeat("a", 5)
 	fmt.Println(result)
 	// Output: aaaaa
 }
